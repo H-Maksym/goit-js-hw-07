d5f2e10e86a174c0083e9c5edea7553acddcb705
Maksym-Holovachuk <mgolovachuk@gmail.com>
2022-07-25 03:30:28 +0300

    task done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const pictureContainer = document.querySelector('.gallery')
+const pictureMarkup = createPictureCardMarkup(galleryItems);
+pictureContainer.insertAdjacentHTML("beforeend", pictureMarkup)
+pictureContainer.addEventListener('click', onPictureShowModalClick);
 
 
-console.log(_.add(2, 3));
-console.log(_.multiply(2, 5));
+function createPictureCardMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>`
+    }).join("");
+};
+
+
+function onPictureShowModalClick(e) {
+    e.preventDefault();
+
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const pictureCard = basicLightbox.create(
+        `<img src="${e.target.dataset.source}" width="800" height="600">`);
+    pictureCard.show();
+
+    if (pictureCard.visible()) {
+        pictureContainer.addEventListener('keydown', (e) => {
+            if (e.code === 'Escape') {
+                pictureCard.close()
+            }
+        }
+        );
+    };
+}
+
+
+
+
